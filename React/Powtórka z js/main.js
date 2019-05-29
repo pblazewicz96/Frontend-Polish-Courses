6dd3356acc9ca26b1b4ba18cebfa8f97f4f44e0c
Piotr Błażewicz <pblazewicz96@gmail.com>
2019-05-29 16:48:46 +0200

    Add practical knowlege to react

--- React/Powtórka z js/main.js
+++ React/Powtórka z js/main.js
@@ -1,3 +1,8 @@
 const userAge = [20, 21, 22, 44];
 
 userAge.forEach(age => console.log(`Każdy z nich za rok ma ${age +1}lat`))
+
+let usersTotalAge = 0;
+
+userAge.forEach(age => usersTotalAge += age);
+console.log(usersTotalAge);
